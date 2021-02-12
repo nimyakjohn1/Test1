fe784eb9c6c3fa40b3f96e5d84a2f91f05079272
nimya-aot <nimya.john@aot-technologies.com>
2021-02-12 12:08:59 -0800

    Please make changes to testcases.Feb12

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Test_Jen</description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e14f4463-c692-4f94-9a95-a55e034dbe59</testSuiteGuid>
+   <testCaseLink>
+      <guid>f19b57db-c6b6-4f2a-ba3a-350d615cb3da</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
